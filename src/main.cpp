cb140a278c4c930843b4d39c543c17a7d650962e
Junho Ryu <jryu@users.noreply.github.com>
2024-07-13 22:48:08 -0700

    Read Protocol Buffer configuration file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,38 @@
 #include <cstdlib>
+#include <fcntl.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/text_format.h>
+#include <stdio.h>
+
+#include "config.pb.h"
+
+config::RendererConfig conf;
+
+bool parse_config() {
+  // Verify that the version of the library that we linked
+  // against is compatible with the version of the headers we
+  // compiled against.
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  int fd = open("config.txt", O_RDONLY);
+  if (fd < 0) {
+    // TODO: Log error message.
+    return false;
+  }
+  google::protobuf::io::FileInputStream fileInput(fd);
+  fileInput.SetCloseOnDelete( true );
+
+  if (!google::protobuf::TextFormat::Parse(&fileInput, &conf)) {
+    // protobuf prints error message
+    return false;
+  }
+  return true;
+}
 
 int main(int argc, char *argv[]) {
+  if (!parse_config()) {
+    return EXIT_FAILURE;
+  }
+
   return EXIT_SUCCESS;
 }
